3959d48273a42a9118030e07913110b91fe9a738
Luca Castri <lucacastr94@gmail.com>
2022-06-02 16:27:18 +0100

    _UAI folder added

---
+++ human_moving_obstacle_UAI/animation.m
@@ -0,0 +1,32 @@
+% close all
+
+%% VIDEO
+answer = questdlg('Do you want to make a video?', ...
+         'Waiting', ...
+         'Yes','No','');
+switch answer
+    case 'Yes'
+        makeVideo = true;
+        if ~exist('video', 'dir')
+            mkdir('video')
+        end
+        video = VideoWriter('video/simulation.avi');
+        video.FrameRate = 50;
+        video.Quality = 85;
+        open(video)
+        ax = gca();
+    otherwise
+        makeVideo = false;
+        video = nan;
+end
+
+%% PLOT
+step = 1; % to change to speed up the video
+
+for t = 1 : step : length(tout)
+    plot_situation(makeVideo, video, boundaries, [G; U], t, axis_def)
+end
+
+if makeVideo
+    close(video)
+end
